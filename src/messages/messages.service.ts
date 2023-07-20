37196dc4200ff695c912e9a16469cc2369645fe2
daisymae <daisymae.orcadiafarm@gmail.com>
2023-07-20 14:25:54 -0500

    Services and Repo
    Start preparing for services and repository.

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,7 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepository {
+    async findOne(id: string) {}
+    async findAll() {}
+    async create(message: string) {}
+}
