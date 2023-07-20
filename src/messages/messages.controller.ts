7983012ead53a1bc6377a021b9761f549686f19d
daisymae <daisymae.orcadiafarm@gmail.com>
2023-07-20 14:44:21 -0500

    The wrong way
    Service and Controller creating own dependencies - DO NOT DO THIS IN REAL APP

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,17 +1,32 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // Controller is creating its own dependencies
+    // DO NOT DO THIS IN REAL APP
+    // USE DEPENDENCY INJECTION
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() { }
+  listMessages() { 
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
     console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
     console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
